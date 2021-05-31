41435f07300c31d4edd8ba3080701e800c52dd6d
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2021-05-31 18:03:31 -0300

    Create Aritméticos.py

---
+++ Operadores/Aritméticos.py
@@ -0,0 +1,28 @@
+#Operadores Aritméticos
+
+num1 = int(45)
+num2 = int(2)
+ 
+adiacao = num1 + num2 # Adição
+sub = num1 - num2 # Subtração
+mult = num1 * num2 # Multiplicação
+div = num1 / num2 # Divisão Exata
+divinteira = num1 // num2 # Divisão Inteira
+mod = num1 % num2  # Módulo (Resto da div)
+exp = num1**num2 # Exponenciação
+
+print(adiacao)
+print(sub)
+print(mult)
+print(div)
+print(divinteira)
+print(mod)
+print(exp)
+
+# Exercícios
+
+# 1- Faça um Programa que peça dois números e imprima a soma.
+
+# 2- Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+# 3- Faça um Programa que converta metros para centímetros.
