250c6e6d247a31a49227a83d95dffb42221d9ad3
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2021-05-31 18:03:01 -0300

    Create Variáveis.py

---
+++ Variáveis/Variáveis.py
@@ -0,0 +1,36 @@
+#Como declarar uma variável:
+
+numero = 15
+
+#Como não de declarar uma variável:
+
+0 = x #Começando com número
+$saldo = 1000 #Começando com símbolos
+
+#Tipos de Variáveis:
+
+num1 = int(15) #Variável do tipo Inteira
+num2 = float(3.14) #Variável do tipo Float
+boo = False #Variável do tipo Booleano
+string = str("Matheus") #Variável do tipo String
+
+#Mostrando esses valores:
+
+print('Variável do tipo inteira', num1)
+print('Variável do tipo ponto flutuante', num2)
+print('Variável do tipo booleano', boo)
+print('Variável do tipo string', string)
+
+#Recebendo um valor:
+
+console = int(input('Digite um valor: '))
+
+#Lembrando sempre definir o tipo da variável ao receber
+
+#Mostrando esse valor:
+
+print(console)
+
+# Exercício
+
+# 1- Criar um programa que recebar do console todos os tipos de variáveis e mostre usando print os seus respestivos valores.
