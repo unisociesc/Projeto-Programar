99513a1ffeda991ab18960fd33844ee59d2bb531
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2021-05-31 18:03:55 -0300

    Create Atribuição.py

---
+++ Operadores/Atribuição.py
@@ -0,0 +1,23 @@
+#Operadores de Atribução
+
+adiacao = int(5)
+sub = int(5)
+mult = int(5)
+div = int(5)
+mod = int(5)
+
+adiacao += 5
+sub -= 5
+mult *= 5
+div /= 5
+mod %= 5
+
+print(adiacao)
+print(sub)
+print(mult)
+print(div)
+print(mod)
+
+# Exercício
+
+# 1 - Faça um programa que some valores utilizando o operador de atribuição.
