f5bcb3f0a5245ab04272e9a578d28b5161bc06ad
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2021-06-23 12:15:45 -0300

    Create Comparação.py

---
+++ Operadores/Comparação.py
@@ -0,0 +1,15 @@
+#Operadores de Comparação
+
+num1 = int(10)
+num2 = int(10)
+
+print('10 é menor que 10 ?', num1<num2)
+print('Num1 é maior que Num2 ?', num1>num2)
+print('Num1 é igual ao Num2 ?', num1==num2)
+print('Num1 é diferente do Num2 ?', num1!=num2)
+print('Num1 é menor ou igual ao Num2 ?', num1<=num2)
+print('Num1 é maior ou igual ao Num2 ?', num1>=num2)
+
+#Exercício
+
+#1- Criar um programa que verifique se 3 números são iguais ou diferentes.
