4158d99b9a9285ea4589ea9561cf7a865c6a9ab4
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2021-06-23 12:26:52 -0300

    Create Logicos.py

---
+++ Operadores/Logicos.py
@@ -0,0 +1,9 @@
+#Operadores Lógicos
+
+num1 = int(10)
+num2 = int(20)
+num3 = int(30)
+
+print(num1<num2 and num2<num3) #Operador and (e)
+print(num1>num3 or num3>num2) #Operador or (ou)
+print(not(num1<num2 and num2<num3)) #Operador not (não)
