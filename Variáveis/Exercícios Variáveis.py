e8c26e3ccb9f357d11aa265336313fe4d502ed75
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2021-06-25 16:31:32 -0300

    Create Exercícios Variáveis.py

---
+++ Variáveis/Exercícios Variáveis.py
@@ -0,0 +1,13 @@
+#Exercícios Variáveis
+
+# 1- Criar um programa que receba do console todos os tipos de variáveis e mostre usando print os seus respestivos valores.
+
+nome = str(input('Digite o seu nome: '))
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite sua altura: '))
+teste = bool(input('Digite True caso goste de programar, ou False caso não goste: '))
+
+print(nome)
+print(idade)
+print(altura)
+print(teste)
